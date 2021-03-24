47a7b3db459eec9225b8a0be882557a8788ad083
Kirandeep <kirandehal612@gmail.com>
2021-03-24 12:47:24 +0530

    Upgrad ST-3 files

---
+++ project.js
@@ -0,0 +1,21 @@
+
+ var count=0;
+ function show()
+{
+  var name=document.getElementById("fname").value;
+  var mob=document.getElementById("cell").value;
+  var mail=document.getElementById("email").value;
+   count++;
+  
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(count);
+  var col1 = row.insertCell(0);
+  var col2 = row.insertCell(1);
+  var col3 = row.insertCell(2);
+  col1.innerHTML = name;
+  col2.innerHTML = mob;
+  col3.innerHTML = mail;
+  
+  document.getElementById("myForm").reset();
+}
+ 
